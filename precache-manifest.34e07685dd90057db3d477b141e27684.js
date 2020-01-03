3a1a0da848b88de33b39edff483c5acb733d77d4
Tetiana <tanichk@ukr.net>
2020-01-03 11:13:30 +0200

    Updates

---
+++ precache-manifest.34e07685dd90057db3d477b141e27684.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc73569df2582d2819fdaed6f3e14369",
+    "url": "/my-task-app/index.html"
+  },
+  {
+    "revision": "e0f017b2ae1f67f28979",
+    "url": "/my-task-app/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "d2f8b677d10e0d24b153",
+    "url": "/my-task-app/static/css/main.7b0929c1.chunk.css"
+  },
+  {
+    "revision": "e0f017b2ae1f67f28979",
+    "url": "/my-task-app/static/js/2.9ff4e561.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/my-task-app/static/js/2.9ff4e561.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d2f8b677d10e0d24b153",
+    "url": "/my-task-app/static/js/main.dbdb303c.chunk.js"
+  },
+  {
+    "revision": "09ea6461a96f00c960cf",
+    "url": "/my-task-app/static/js/runtime-main.c7ab1d15.js"
+  }
+]);
